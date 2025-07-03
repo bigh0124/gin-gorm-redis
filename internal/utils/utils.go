aa1a5e02596c24a2c96857e65d51b01e4c3c3c31
bigh0124 <thubgi0629@gmail.com>
2025-07-03 17:58:30 +0800

    feat: 新增generateJWT utils

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,6 +1,11 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
@@ -9,4 +14,14 @@ func HashPassword(password string) (string, error) {
 	}
 
 	return string(hash), nil
+}
+
+func GenerateJWT(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	signedToken, err := token.SignedString([]byte("secret"))
+	return signedToken, err
 }
